291c5ff5cf4733b8f344b51a988cfc222d1c618d
Laila Morsli <145076507+ilamor@users.noreply.github.com>
2023-09-15 02:12:53 +0200

    Add files via upload

---
+++ camagru/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+    <title>Camagru</title>
+</head>
+<body>
+    <header>
+        <div class="connexion">
+            <form name="login" method="post" action="signup.php">
+                <input name="login" type="submit" value="Connexion">
+            </form>
+        </div>
+    </header>
+<?php
+    echo "ceci est un test";
+?>
+</body>
+</html>
